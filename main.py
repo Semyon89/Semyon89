484fc5adebd4d8a9a462ef5597e089e9274011e1
Victor <AkhterovSm@yandex.ru>
2023-06-28 16:05:21 +0500

    added def palindrome(s)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-def strcounter(s):
-    syms_counter = {}
-    for sym in s:
-        syms_counter[sum] =  syms_counter.get(sym, 0) +1
+# def strcounter(s):
+#     syms_counter = {}
+#     for sym in s:
+#         syms_counter[sum] =  syms_counter.get(sym, 0) +1
+#
+#         print(syms_counter)
+#
+# strcounter('aaaabca')
 
-        print(syms_counter)
 
-strcounter('aaaabca')
+def palindrome(s):
+    return s[::-1] == s
+
+while True:
+
+    s = input("введите слово: ")
+    print(f"{s} это слово является палиндромом" if
+palindrome (s) else "это слово не является палиндромом")
